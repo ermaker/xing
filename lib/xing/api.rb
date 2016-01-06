a1e11f272e62d486055a90f751b2eb96cd1ede87
Minwoo Lee <ermaker@gmail.com>
2016-01-07 08:59:18 +0900

    Add logging support for unexpected message codes

--- lib/xing/api.rb
+++ lib/xing/api.rb
@@ -1,11 +1,16 @@
 require 'httparty'
 require 'multi_json'
+require 'logger'
 
 module Xing
   class API
     include HTTParty
     class << self
       attr_accessor :base
+
+      def logger
+        @logger ||= Logger.new(STDOUT)
+      end
 
       def json(uri, **args)
         post(uri,
@@ -18,14 +23,29 @@ module Xing
         get(base + "/account/#{idx}")
       end
 
-      def tr(tr_name, **args)
+      def manipulate_shcode(args)
         case args[:shcode]
         when :leverage
           args[:shcode] = '122630'
         when :inverse
           args[:shcode] = '114800'
         end
-        json(base + "/tr/#{tr_name}", args)
+      end
+
+      def log_if_unexpected(retval, white_code)
+        return if white_code.include? retval['message'][/^\[(.+?)\]/, 1]
+        logger.warn { "Check: #{retval['message']}" }
+      end
+
+      TR_WHITE_CODE = [
+        # '00000'
+      ]
+
+      def tr(tr_name, **args)
+        manipulate_shcode(args)
+        json(base + "/tr/#{tr_name}", args).tap do |retval|
+          log_if_unexpected(retval, TR_WHITE_CODE)
+        end
       end
     end
     self.base = ENV['XING_URI'] + '/v0'
